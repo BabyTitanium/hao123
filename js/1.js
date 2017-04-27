f8b2563e807c881f65fc6c03722975541512dadf
llling <in19951209@163.com>
2017-04-27 18:36:55 +0800

    看一下

---
+++ js/1.js
@@ -0,0 +1,175 @@
+
+					function change1() {
+						var x=document.getElementById('le');
+						var y=document.getElementById('ri');
+						var f1=document.getElementById('fon1');
+						var f2=document.getElementById('fon2');
+						x.style="border-top:2px solid green;";
+					    y.style="border-top:0;";
+					    f1.style="color:green;font-weight:600;font-size:16;"
+					    f2.style="color:black;font-weight:400;font-size:16;"
+					    var l=document.getElementById('con1');
+					    l.style="display:run-in;width: 230px;height: 320;background-color: grey;padding-left";
+					    var r=document.getElementById('con2');
+					    r.style="display:none;"
+					}
+						function change2() {
+						var x=document.getElementById('le');
+						var y=document.getElementById('ri');
+						var f1=document.getElementById('fon1');
+						var f2=document.getElementById('fon2');
+						y.style="border-top:2px solid green;";
+					    x.style="border-top:0;";
+					    f2.style="color:green;font-weight:600;font-size:16;"
+					    f1.style="color:black;font-weight:400;font-size:16;"
+					      var l=document.getElementById('con1');
+					    l.style="display:none;";
+					    var r=document.getElementById('con2');
+					    r.style="display:run-in;width: 230px;height: 320;background-color: pink;"
+					}
+			
+					function change11() {
+						var x=document.getElementById('le1');
+						var y=document.getElementById('ri1');
+						var f1=document.getElementById('fon11');
+						var f2=document.getElementById('fon21');
+						x.style="border-top:2px solid green;";
+					    y.style="border-top:0;";
+					    f1.style="color:green;font-weight:600;font-size:16;"
+					    f2.style="color:black;font-weight:400;font-size:16;"
+					    var l=document.getElementById('con11');
+					    l.style="display:run-in;width: 230px;height: 320;background-color: grey;padding-left";
+					    var r=document.getElementById('con21');
+					    r.style="display:none;"
+					}
+						function change21() {
+						var x=document.getElementById('le1');
+						var y=document.getElementById('ri1');
+						var f1=document.getElementById('fon11');
+						var f2=document.getElementById('fon21');
+						y.style="border-top:2px solid green;";
+					    x.style="border-top:0;";
+					    f2.style="color:green;font-weight:600;font-size:16;"
+					    f1.style="color:black;font-weight:400;font-size:16;"
+					      var l=document.getElementById('con11');
+					    l.style="display:none;";
+					    var r=document.getElementById('con21');
+					    r.style="display:run-in;width: 230px;height: 320;background-color: pink;"
+					}
+					
+					function change13() {
+						var x=document.getElementById('le3');
+						var y=document.getElementById('ri3');
+						var f1=document.getElementById('fon13');
+						var f2=document.getElementById('fon23');
+						x.style="border-top:2px solid green;";
+					    y.style="border-top:0;";
+					    f1.style="color:green;font-weight:600;font-size:16;"
+					    f2.style="color:black;font-weight:400;font-size:16;"
+					    var l=document.getElementById('con13');
+					    l.style="display:run-in;width: 230px;height: 320;background-color: grey;padding-left";
+					    var r=document.getElementById('con23');
+					    r.style="display:none;"
+					}
+						function change23() {
+						var x=document.getElementById('le3');
+						var y=document.getElementById('ri3');
+						var f1=document.getElementById('fon13');
+						var f2=document.getElementById('fon23');
+						y.style="border-top:2px solid green;";
+					    x.style="border-top:0;";
+					    f2.style="color:green;font-weight:600;font-size:16;"
+					    f1.style="color:black;font-weight:400;font-size:16;"
+					      var l=document.getElementById('con13');
+					    l.style="display:none;";
+					    var r=document.getElementById('con23');
+					    r.style="display:run-in;width: 230px;height: 320;background-color: pink;"
+					}
+		
+				
+					function change14() {
+						var x=document.getElementById('le4');
+						var y=document.getElementById('ri4');
+						var f1=document.getElementById('fon14');
+						var f2=document.getElementById('fon24');
+						x.style="border-top:2px solid green;";
+					    y.style="border-top:0;";
+					    f1.style="color:green;font-weight:600;font-size:16;"
+					    f2.style="color:black;font-weight:400;font-size:16;"
+					    var l=document.getElementById('con14');
+					    l.style="display:run-in;width: 230px;height: 320;background-color: grey;padding-left";
+					    var r=document.getElementById('con24');
+					    r.style="display:none;"
+					}
+						function change24() {
+						var x=document.getElementById('le4');
+						var y=document.getElementById('ri4');
+						var f1=document.getElementById('fon14');
+						var f2=document.getElementById('fon24');
+						y.style="border-top:2px solid green;";
+					    x.style="border-top:0;";
+					    f2.style="color:green;font-weight:600;font-size:16;"
+					    f1.style="color:black;font-weight:400;font-size:16;"
+					      var l=document.getElementById('con14');
+					    l.style="display:none;";
+					    var r=document.getElementById('con24');
+					    r.style="display:run-in;width: 230px;height: 320;background-color: pink;"
+					}
+				
+					function change15() {
+						var x=document.getElementById('le5');
+						var y=document.getElementById('ri5');
+						var f1=document.getElementById('fon15');
+						var f2=document.getElementById('fon25');
+						x.style="border-top:2px solid green;";
+					    y.style="border-top:0;";
+					    f1.style="color:green;font-weight:600;font-size:16;"
+					    f2.style="color:black;font-weight:400;font-size:16;"
+					    var l=document.getElementById('con15');
+					    l.style="display:run-in;width: 230px;height: 320;background-color: grey;padding-left";
+					    var r=document.getElementById('con25');
+					    r.style="display:none;"
+					}
+						function change25() {
+						var x=document.getElementById('le5');
+						var y=document.getElementById('ri5');
+						var f1=document.getElementById('fon15');
+						var f2=document.getElementById('fon25');
+						y.style="border-top:2px solid green;";
+					    x.style="border-top:0;";
+					    f2.style="color:green;font-weight:600;font-size:16;"
+					    f1.style="color:black;font-weight:400;font-size:16;"
+					      var l=document.getElementById('con15');
+					    l.style="display:none;";
+					    var r=document.getElementById('con25');
+					    r.style="display:run-in;width: 230px;height: 320;background-color: pink;"
+					}
+		
+						function color(v){
+							
+							
+							v.style="background-color:green;border-right: 2px solid grey;width: 123px;float: left;text-align: center;border-right: 2px solid grey;height: 30px;line-height: 30px;font-family: 宋体;font-size: 15;";
+							//var t=document.getElementById('p11');
+						//	t.style.border-right="none";
+						
+						}
+						function color2(v){
+							//document.getElementById('p11').style="background-color:red;width: 123px;float: left;text-align: center;height: 30px;line-height: 30px;font-family: 宋体;font-size: 15;";
+							v.style="border-right: 2px solid grey;width: 123px;float: left;text-align: center;border-right: 2px solid grey;height: 30px;line-height: 30px;font-family: 宋体;font-size: 15;";
+							
+						}
+							function color1(v){
+							
+							
+						v.style="background-color:green;width: 123px;float: left;text-align: center;height: 30px;line-height: 30px;font-family: 宋体;font-size: 15;";
+							//var t=document.getElementById('p11');
+						//	t.style.border-right="none";
+						
+						}
+						function color21(v){
+							//document.getElementById('p11').style="background-color:red;width: 123px;float: left;text-align: center;height: 30px;line-height: 30px;font-family: 宋体;font-size: 15;";
+							v.style="border-right:none;width: 123px;float: left;text-align: center;height: 30px;line-height: 30px;font-family: 宋体;font-size: 15;";
+							
+						}
+				
+			
